df9d0d25dfa691c4e5bc95482dca90f551c1fff5
saffr3n <91146584+saffr3n@users.noreply.github.com>
2026-07-11 00:22:06 +0300

    feat(scanner): scan simple two-character lexemes

--- Scanner.java
+++ Scanner.java
@@ -35,8 +35,27 @@ public class Scanner {
       case '+': addToken(TokenType.PLUS);        break;
       case ';': addToken(TokenType.SEMICOLON);   break;
       case '*': addToken(TokenType.STAR);        break;
+      case '!':
+        addToken(match('=') ? TokenType.BANG_EQUAL : TokenType.BANG);
+        break;
+      case '=':
+        addToken(match('=') ? TokenType.EQUAL_EQUAL : TokenType.EQUAL);
+        break;
+      case '<':
+        addToken(match('=') ? TokenType.LESS_EQUAL : TokenType.LESS);
+        break;
+      case '>':
+        addToken(match('=') ? TokenType.GREATER_EQUAL : TokenType.GREATER);
+        break;
       default: Lox.error(line, "Unexpected character");
     }
+  }
+
+  private boolean match(char expected) {
+    if (isAtEnd()) return false;
+    if (source.charAt(current) != expected) return false;
+    current++;
+    return true;
   }
 
   private boolean isAtEnd() {
