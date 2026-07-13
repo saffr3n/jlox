9ce52c7df839bdb879d38354c6b9172a09821ddc
saffr3n <91146584+saffr3n@users.noreply.github.com>
2026-07-14 01:04:20 +0300

    feat(parser): parse literal expressions

--- Parser.java
+++ Parser.java
@@ -1,13 +1,70 @@
 import java.util.List;
 
 public class Parser {
+  private static class ParseError extends RuntimeException {}
+
   private final List<Token> tokens;
+  private int current = 0;
 
   public Parser(List<Token> tokens) {
     this.tokens = tokens;
   }
 
   public Expr parse() {
-    return null;
+    try {
+      return primary();
+    } catch (ParseError error) {
+      return null;
+    }
+  }
+
+  private Expr primary() {
+    if (match(TokenType.FALSE)) return new Expr.Literal(false);
+    if (match(TokenType.TRUE)) return new Expr.Literal(true);
+    if (match(TokenType.NIL)) return new Expr.Literal(null);
+
+    if (match(TokenType.NUMBER, TokenType.STRING)) {
+      return new Expr.Literal(previous().literal);
+    }
+
+    throw error(peek(), "Expect expression");
+  }
+
+  private boolean match(TokenType... types) {
+    for (TokenType type : types) {
+      if (check(type)) {
+        advance();
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  private boolean check(TokenType type) {
+    if (isAtEnd()) return false;
+    return peek().type == type;
+  }
+
+  private Token advance() {
+    if (!isAtEnd()) current++;
+    return previous();
+  }
+
+  private boolean isAtEnd() {
+    return peek().type == TokenType.EOF;
+  }
+
+  private Token peek() {
+    return tokens.get(current);
+  }
+
+  private Token previous() {
+    return tokens.get(current - 1);
+  }
+
+  private ParseError error(Token token, String message) {
+    Lox.error(token, message);
+    return new ParseError();
   }
 }
