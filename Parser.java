6eed57e0fcd860b07ea565a265cc9d6b5d7c4452
saffr3n <91146584+saffr3n@users.noreply.github.com>
2026-07-14 00:55:47 +0300

    feat(parser): add Parser class

---
+++ Parser.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public class Parser {
+  private final List<Token> tokens;
+
+  public Parser(List<Token> tokens) {
+    this.tokens = tokens;
+  }
+
+  public Expr parse() {
+    return null;
+  }
+}
