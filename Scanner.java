76c88773089b19cf60c8496727d6dc75c343d2d5
saffr3n <91146584+saffr3n@users.noreply.github.com>
2026-07-14 20:29:15 +0300

    feat(scanner): scan ? and : tokens

--- Scanner.java
+++ Scanner.java
@@ -52,10 +52,12 @@ public class Scanner {
       case ')': addToken(TokenType.RIGHT_PAREN); break;
       case '{': addToken(TokenType.LEFT_BRACE);  break;
       case '}': addToken(TokenType.RIGHT_BRACE); break;
+      case ':': addToken(TokenType.COLON);       break;
       case ',': addToken(TokenType.COMMA);       break;
       case '.': addToken(TokenType.DOT);         break;
       case '-': addToken(TokenType.MINUS);       break;
       case '+': addToken(TokenType.PLUS);        break;
+      case '?': addToken(TokenType.QUESTION);    break;
       case ';': addToken(TokenType.SEMICOLON);   break;
       case '*': addToken(TokenType.STAR);        break;
       case '!':
