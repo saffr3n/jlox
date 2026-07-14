1ff0fd91e4d3758880630a7ae52b1842a0900a44
saffr3n <91146584+saffr3n@users.noreply.github.com>
2026-07-14 19:49:27 +0300

    feat(parser): parse comma expressions

--- Parser.java
+++ Parser.java
@@ -19,7 +19,19 @@ public class Parser {
   }
 
   private Expr expression() {
-    return equality();
+    return comma();
+  }
+
+  private Expr comma() {
+    Expr expr = equality();
+
+    while (match(TokenType.COMMA)) {
+      Token operator = previous();
+      Expr right = equality();
+      expr = new Expr.Binary(expr, operator, right);
+    }
+
+    return expr;
   }
 
   private Expr equality() {
