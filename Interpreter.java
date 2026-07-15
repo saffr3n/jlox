4663a04d68e5f305248a678d5aee7dbe34d11b35
saffr3n <91146584+saffr3n@users.noreply.github.com>
2026-07-16 00:52:07 +0300

    feat(interpreter): forbid division by zero

--- Interpreter.java
+++ Interpreter.java
@@ -43,6 +43,7 @@ public class Interpreter implements Expr.Visitor<Object> {
         throw new RuntimeError(expr.operator, "Operands must be two numbers or two strings");
       case TokenType.SLASH:
         checkNumberOperands(expr.operator, left, right);
+        if ((double)right == 0) throw new RuntimeError(expr.operator, "Cannot divide by zero");
         return (double)left / (double)right;
       case TokenType.STAR:
         checkNumberOperands(expr.operator, left, right);
