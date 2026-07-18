738a4c7e7e3c4c8a4e668b16c78333532203dd35
saffr3n <91146584+saffr3n@users.noreply.github.com>
2026-07-19 01:00:26 +0300

    feat(parser, interpreter): add global var declarations

--- Parser.java
+++ Parser.java
@@ -14,9 +14,31 @@ public class Parser {
   public List<Stmt> parse() {
     List<Stmt> statements = new ArrayList<>();
     while (!isAtEnd()) {
-      statements.add(statement());
+      statements.add(declaration());
     }
     return statements;
+  }
+
+  private Stmt declaration() {
+    try {
+      if (match(TokenType.VAR)) return varDeclaration();
+      return statement();
+    } catch (ParseError error) {
+      synchronize();
+      return null;
+    }
+  }
+
+  private Stmt varDeclaration() {
+    Token name = consume(TokenType.IDENTIFIER, "Expect variable name");
+
+    Expr initializer = null;
+    if (match(TokenType.EQUAL)) {
+      initializer = expression();
+    }
+
+    consume(TokenType.SEMICOLON, "Expect ';' after variable declaration");
+    return new Stmt.Var(name, initializer);
   }
 
   private Stmt statement() {
@@ -105,6 +127,10 @@ public class Parser {
 
     if (match(TokenType.NUMBER, TokenType.STRING)) {
       return new Expr.Literal(previous().literal);
+    }
+
+    if (match(TokenType.IDENTIFIER)) {
+      return new Expr.Variable(previous());
     }
 
     if (match(TokenType.LEFT_PAREN)) {
