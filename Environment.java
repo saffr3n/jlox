ccef3525d2cfc2c3f904c0037ebca8286667641e
saffr3n <91146584+saffr3n@users.noreply.github.com>
2026-07-21 00:23:15 +0300

    feat(environment): add enclosing environment

--- Environment.java
+++ Environment.java
@@ -2,7 +2,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Environment {
+  public final Environment enclosing;
   private final Map<String, Object> values = new HashMap<>();
+
+  public Environment() {
+    enclosing = null;
+  }
+
+  public Environment(Environment enclosing) {
+    this.enclosing = enclosing;
+  }
 
   public void define(String name, Object value) {
     values.put(name, value);
@@ -14,6 +23,11 @@ public class Environment {
       return;
     }
 
+    if (enclosing != null) {
+      enclosing.assign(name, value);
+      return;
+    }
+
     throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'");
   }
 
@@ -21,6 +35,8 @@ public class Environment {
     if (values.containsKey(name.lexeme)) {
       return values.get(name.lexeme);
     }
+
+    if (enclosing != null) return enclosing.get(name);
 
     throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'");
   }
