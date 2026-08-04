002b5c064c2c38b449fefb75b8022e68df73c848
saffr3n <91146584+saffr3n@users.noreply.github.com>
2026-08-05 00:36:28 +0300

    feat(class): add LoxInstance class

---
+++ LoxInstance.java
@@ -0,0 +1,12 @@
+public class LoxInstance {
+  private LoxClass klass;
+
+  public LoxInstance(LoxClass klass) {
+    this.klass = klass;
+  }
+
+  @Override
+  public String toString() {
+    return klass.name + " instance";
+  }
+}
