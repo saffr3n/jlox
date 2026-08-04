7f76a1d61a831417c16401fb54225178c68f57e4
saffr3n <91146584+saffr3n@users.noreply.github.com>
2026-08-05 00:37:05 +0300

    feat(interpreter): interpret class declarations

--- Interpreter.java
+++ Interpreter.java
@@ -197,6 +197,14 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
   }
 
   @Override
+  public Void visitClassStmt(Stmt.Class stmt) {
+    environment.define(stmt.name.lexeme, null);
+    LoxClass klass = new LoxClass(stmt.name.lexeme);
+    environment.assign(stmt.name, klass);
+    return null;
+  }
+
+  @Override
   public Void visitFunctionStmt(Stmt.Function stmt) {
     LoxFunction function = new LoxFunction(stmt, environment);
     environment.define(stmt.name.lexeme, function);
