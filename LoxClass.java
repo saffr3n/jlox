ad0231f65a800546a3c4fae85e15e1d6420fb16e
saffr3n <91146584+saffr3n@users.noreply.github.com>
2026-08-05 00:36:07 +0300

    feat(callable, class): add LoxClass class

---
+++ LoxClass.java
@@ -0,0 +1,25 @@
+import java.util.List;
+
+public class LoxClass implements LoxCallable {
+  public final String name;
+
+  public LoxClass(String name) {
+    this.name = name;
+  }
+
+  @Override
+  public Object call(Interpreter interpreter, List<Object> arguments) {
+    LoxInstance instance = new LoxInstance(this);
+    return instance;
+  }
+
+  @Override
+  public int arity() {
+    return 0;
+  }
+
+  @Override
+  public String toString() {
+    return name;
+  }
+}
