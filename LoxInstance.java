3f1248536eb99d0d44df83a59b157a275ffb4551
saffr3n <91146584+saffr3n@users.noreply.github.com>
2026-08-06 00:15:50 +0300

    feat(class): add LoxInstance.get() method

--- LoxInstance.java
+++ LoxInstance.java
@@ -1,8 +1,20 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class LoxInstance {
+  private final Map<String, Object> fields = new HashMap<>();
+
   private LoxClass klass;
 
   public LoxInstance(LoxClass klass) {
     this.klass = klass;
+  }
+
+  public Object get(Token name) {
+    if (fields.containsKey(name.lexeme)) {
+      return fields.get(name.lexeme);
+    }
+    throw new RuntimeError(name, "Undefined property " + name.lexeme);
   }
 
   @Override
