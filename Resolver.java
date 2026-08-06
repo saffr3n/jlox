5daea44a3df604972f17551c869fca48aabcbad8
saffr3n <91146584+saffr3n@users.noreply.github.com>
2026-08-07 01:44:06 +0300

    feat(resolver): resolve class methods

--- Resolver.java
+++ Resolver.java
@@ -11,6 +11,7 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
   private enum FunctionType {
     NONE,
     FUNCTION,
+    METHOD,
   }
 
   public Resolver(Interpreter interpreter) {
@@ -35,6 +36,10 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
   public Void visitClassStmt(Stmt.Class stmt) {
     declare(stmt.name);
     define(stmt.name);
+    for (Stmt.Function method : stmt.methods) {
+      FunctionType declaration = FunctionType.METHOD;
+      resolveFunction(method, declaration);
+    }
     return null;
   }
 
